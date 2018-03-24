02a7549677e79a8f00fb02408915c3e5225dc946
AngusChen <hiaiokr@gmail.com>
2018-03-24 22:41:20 +0800

    add

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 header('access-control-allow-origin:*');
 //获取包含句子的文件
 $path = dirname(__FILE__);
-$file = file($path."https://raw.githubusercontent.com/aiokr/hitokoto/master/hitokoto.txt");
+$file = file($path."./hitokoto.txt");
 
 //获取一句句子
 $arr  = mt_rand( 0, count( $file ) - 1 );
