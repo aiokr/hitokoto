4233419a60ca161c53255a841d81157230f0ae83
aiokr <aiokr@foxmail.com>
2018-03-24 22:06:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+header('access-control-allow-origin:*');
+//获取包含句子的文件
+$path = dirname(__FILE__);
+$file = file($path."https://raw.githubusercontent.com/aiokr/hitokoto/master/hitokoto.txt");
+
+//获取一句句子
+$arr  = mt_rand( 0, count( $file ) - 1 );
+$hitokoto  = trim($file[$arr]);
+
+//输出内容
+echo $hitokoto;
+?>
